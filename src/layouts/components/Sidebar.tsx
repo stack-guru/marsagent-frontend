c914f5b3cff6c1cdbee81890ae41331243721fe8
stack-guru <GoldenHand0302@gmail.com>
2025-07-02 16:28:20 -0500

    refactor: update home and sidebar

--- src/layouts/components/Sidebar.tsx
+++ src/layouts/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/assets/logo.png";
 import { NavLink } from "react-router-dom";
 import { useRoot } from "../../hooks/RootContext";
+import { FaTelegram, FaTwitter, FaBook } from "react-icons/fa";
 
 export default function Sidebar() {
   const {
@@ -26,8 +27,9 @@ export default function Sidebar() {
               <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"></path>
             </svg>
           </span>
-          <div>
-            <img src={Logo} alt="Logo" />
+          <div className="text-center">
+            <img src={Logo} alt="Logo" width={100} />
+            <p className="text-white font-semibold">Marsagent</p>
           </div>
         </div>
         <div className="my-[30px] border-t border-[#ddd] "></div>
@@ -143,6 +145,17 @@ export default function Sidebar() {
             </div>
           </div>
         </div> */}
+        <div className="absolute text-white bottom-5 flex items-center justify-center gap-4">
+          <a href="#" target="_blank" className="hover:text-gray-500 text-white">
+            <FaTelegram size={32} />
+          </a>
+          <a href="#" target="_blank" className="hover:text-gray-500 text-white">
+            <FaTwitter size={32} />
+          </a>
+          <a href="#" target="_blank" className="hover:text-gray-500 text-white">
+            <FaBook size={32} />
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home/components/Avatar.tsx
+++ src/pages/Home/components/Avatar.tsx
@@ -3,6 +3,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 // import AvatarImg from "@/assets/avatar.png";
 import Logo from "@/assets/logo.png";
+import { FaLink, FaRobot, FaPlus } from "react-icons/fa";
 
 export default function Avatar() {
   useEffect(() => {
@@ -22,10 +23,10 @@ export default function Avatar() {
           />
         </div>
         <div
-          className="pulse bg-card absolute bottom-[-50px] right-[40px] flex items-center gap-[20px] rounded-full px-[40px] py-[18px] md:bottom-[-5px] lg:right-[-34px]"
+          className="pulse bg-card absolute bottom-[-10px] right-[10px] flex items-center gap-[20px] rounded-full px-[40px] py-[18px] md:bottom-[-5px] lg:right-[-34px]"
           data-aos="fade-up"
         >
-          <div>
+          {/* <div>
             <svg
               stroke="currentColor"
               fill="currentColor"
@@ -38,10 +39,23 @@ export default function Avatar() {
             >
               <path d="M512 32c0 113.6-84.6 207.5-194.2 222c-7.1-53.4-30.6-101.6-65.3-139.3C290.8 46.3 364 0 448 0h32c17.7 0 32 14.3 32 32zM0 96C0 78.3 14.3 64 32 64H64c123.7 0 224 100.3 224 224v32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V320C100.3 320 0 219.7 0 96z"></path>
             </svg>
-          </div>
-          <div>
+          </div> */}
+          {/* <div>
             <h2 className="text-[26px] font-semibold text-white">11M</h2>
             <p className="text-base text-white">Users</p>
+          </div> */}
+          <div className="text-theme flex items-center gap-2">
+            <FaRobot className="text-theme" size={28}/>
+            <FaPlus className="text-white" size={12}/>
+            <FaLink className="text-theme" size={28}/>
+            {/* <div className="flex items-center gap-4">
+              <FaRobot />
+              <span className="text-white text-[18px] font-semibold">AI</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <FaLink />
+              <span className="text-white text-[18px] font-semibold">Blockchain</span>
+            </div> */}
           </div>
         </div>
       </div>
